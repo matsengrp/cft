531d3443b6f102d14d4633b9e7309208985f6bbb
D. Shaw <wahsadivad@gmail.com>
2016-10-20 16:05:06 -0700

    so git ignores output files

--- post_partis/scripts/demo.sh
+++ post_partis/scripts/demo.sh
@@ -5,12 +5,12 @@ python scripts/process_partis.py \
     --incsv data/seeds/QA255.006-Vh/Hs-LN2-5RACE-IgG-new-cluster-annotations.csv \
     --cluster_base cluster \
     --input_dir data/seeds/ \
-    --output_dir output/seeds/
+    --output_dir _output/seeds/
 
 ## BASELINe-style output
 #python scripts/process_partis.py \
 #    --incsv data/seeds/QA255.006-Vh/Hs-LN2-5RACE-IgG-new-cluster-annotations.csv \
 #    --cluster_base cluster \
 #    --input_dir data/seeds/ \
-#    --output_dir output/seeds/ \
+#    --output_dir _output/seeds/ \
 #    --baseline
