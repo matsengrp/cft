3d29922ec8ac589a7b259ebdfcbe495e45c50fb3
D. Shaw <wahsadivad@gmail.com>
2016-11-03 17:22:56 -0700

    remove baseline example; add separate example

--- post_partis/scripts/demo.sh
+++ post_partis/scripts/demo.sh
@@ -12,15 +12,22 @@ python scripts/process_partis.py \
 
 # Aggregated fasta file with all seqs
 python scripts/process_partis.py \
+    --incsv data/seeds/QA255.006-Vh/Hs-LN2-5RACE-IgG-new-cluster-annotations.csv \
+    --cluster_base cluster \
+    --input_dir data/seeds/ \
+    --output_dir _output/seeds/
+
+# Aggregated fasta file with all seqs
+python scripts/process_partis.py \
     --incsv data/seeds/QB850.424-Vk/Hs-LN1-5RACE-IgK-100k-cluster-annotations.csv \
     --cluster_base cluster \
     --input_dir data/seeds/ \
     --output_dir _output/seeds/
 
-# BASELINe-style output
+# Separate fasta files for each cluster
 python scripts/process_partis.py \
     --incsv data/seeds/QB850.043-Vk/Hs-LN1-5RACE-IgK-100k-cluster-annotations.csv \
     --cluster_base cluster \
     --input_dir data/seeds/ \
     --output_dir _output/seeds/ \
-    --baseline
+    --separate
