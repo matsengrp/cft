248bfaf9d04d6e14719a1b85d827a68d01c78c29
D. Shaw <wahsadivad@gmail.com>
2016-11-07 17:01:44 -0800

    two working instances

--- post_partis/scripts/demo.sh
+++ post_partis/scripts/demo.sh
@@ -7,29 +7,20 @@ seed1="QA255.006-Vh"
 seed2="QB850.424-Vk"
 seed3="QB850.043-Vk"
 
-datapath="data/seeds/"
+datapath="/fh/fast/matsen_e/dshaw/cft/data/seeds"
 
 # Separate fasta files for each cluster
 python scripts/process_partis.py \
-    --incsv $datapath$seed2/Hs-LN1-5RACE-IgK-100k-cluster-annotations.csv \
+    --incsv $datapath/$seed2/Hs-LN1-5RACE-IgK-100k-cluster-annotations.csv \
     --cluster_base cluster \
     --input_dir $datapath \
-    --output_dir _output/seeds/ \
+    --output_dir _output/seeds \
     --separate
-
-## Separate fasta files with multiple seeds.
-## Currently still running partis to get output for this.
-#python scripts/process_partis.py \
-#    --incsv $datapath/$seed2:$seed3/Hs-LN1-5RACE-IgK-100k-cluster-annotations.csv \
-#    --cluster_base cluster \
-#    --input_dir $datapath \
-#    --output_dir _output/seeds/ \
-#    --separate
 
 # Aggregated fasta file with all seqs
 python scripts/process_partis.py \
-    --incsv $datapath$seed1/Hs-LN2-5RACE-IgG-new-cluster-annotations.csv \
+    --incsv $datapath/$seed1/Hs-LN2-5RACE-IgG-new-cluster-annotations.csv \
     --cluster_base cluster \
     --input_dir $datapath \
-    --output_dir _output/seeds/
+    --output_dir _output/seeds
 
