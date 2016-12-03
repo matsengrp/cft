ea220fa69b8fabd6e186930c7e40ced74e136edf
William DeWitt <wsdewitt@gmail.com>
2016-12-02 16:38:33 -0800

    33 alncolor (#47)
    * colored annotation (currently hardcoded indices)
    
    * mock gene indices, mutations, and cdr3
    
    * 24 integration test (#41)
    
    * add new sample data and wercker config file
    
    * build phylib tools
    
    * add wercker badge
    
    * move seed to the top of tree; resolves #42 (#43)
    
    * output v/d/j region bounds
    
    * real alignment annotation, closes 33
    
    * disabled random mutation mock coloring

--- cftweb/cftweb/filters.py
+++ cftweb/cftweb/filters.py
@@ -47,4 +47,37 @@ def unique(a):
                 c.append(x)
     return c
 
+@app.template_filter()
+def annotate(seq, cluster):
+    if len(seq) == 0: return seq
+    seq = seq.upper()
+    # random indices to mock up to 10 mutations in each sequence
+    #import random
+    #lenseq = len(seq)
+    #if lenseq >= 10:
+    #    mutations = set(random.sample(range(lenseq), random.randrange(10)))
+    #else:
+    #    mutations = []
+    mutations = [] # <-- eventually read from metadata
 
+    if cluster.d_start == cluster.d_end:
+        chain = 'light'
+    else:
+        chain = 'heavy'
+    # NOTE: this will make people laugh at you, recode to hierarchical spans
+    foo = ''#seq[:vIndex]
+    for i in range(int(cluster.v_start), (cluster.j_end)):
+        if int(cluster.v_start) <= i < int(cluster.v_end):
+            gene_class = 'Vgene'
+        elif (chain == 'heavy' and (int(cluster.v_end) <= i < int(cluster.d_start) or int(cluster.d_end) <= i < int(cluster.j_start))) or \
+             (chain == 'light' and int(cluster.v_end) <= i < int(cluster.j_start)):
+            gene_class = 'N'
+        elif int(cluster.d_start) <= i < int(cluster.d_end):
+            gene_class = 'Dgene'
+        elif int(cluster.j_start) <= i:
+            gene_class = 'Jgene'
+        foo += '<span class="' + gene_class + \
+               (' mutation' if i in mutations else '') + \
+               (' CDR3' if int(cluster.cdr3_start) <= i < int(cluster.cdr3_start) + int(cluster.cdr3_length) else '') + \
+               '"">' + seq[i] + '</span>'
+    return foo
