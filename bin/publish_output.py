e7ebfa1033e98198cc72efbd8f12f57a785380cc
Christopher Small <csmall@fhcrc.org>
2017-07-20 22:29:12 -0700

    add prefix option to export script

--- bin/publish_output.py
+++ bin/publish_output.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
 import argparse
-#import json
 import os
-import shutil
+import subprocess
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('outdir')
+    parser.add_argument('-p', '--prefix', help="add a prefix")
     parser.add_argument('inputs', nargs='+')
     return parser.parse_args()
 
 def main():
     args = get_args()
     for inpath in args.inputs:
-        outpath = os.path.join(args.outdir, '-'.join(inpath.split('/')[-2:]))
-        shutil.copy(inpath, outpath)
+        name_parts = inpath.split('/')[-2:]
+        if args.prefix:
+            name_parts = [args.prefix] + name_parts
+        outpath = os.path.join(args.outdir, '-'.join(name_parts))
+        subprocess.check_call(['cp', inpath, outpath])
 
 
 if __name__ == '__main__':
