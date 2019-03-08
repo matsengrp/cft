e0a8ac18424593c93a662c437544301ef20a8007
eharkins <eli.harkins@gmail.com>
2019-03-08 14:56:58 -0800

    custom color schemes but import doesn't work

--- bin/create_partis_naive_logo.py
+++ bin/create_partis_naive_logo.py
@@ -21,6 +21,29 @@ if __name__ == '__main__':
     subprocess.check_call(("rm " + args.output_base + ".fasta").split())
 
     options = w.LogoOptions()
+    '''
+    SymbolColor wont import
+    chemistry = w.colorscheme.ColorScheme(
+        [
+            w.colorscheme.SymbolColor("GSTYC", "green", "polar"),
+            w.colorscheme.SymbolColor("NQ", "purple", "neutral"),
+            w.colorscheme.SymbolColor("KRH", "blue", "basic"),
+            w.colorscheme.SymbolColor("DE", "red", "acidic"),
+            w.colorscheme.SymbolColor("PAWFLIMV", "black", "hydrophobic")
+        ],
+        alphabet=w.seq.unambiguous_protein_alphabet
+    )
+    bloom = ColorScheme(
+        [
+            SymbolColor("GSTYC", "green", "polar"),
+            SymbolColor("NQ", "purple", "neutral"),
+            SymbolColor("KRH", "blue", "basic"),
+            SymbolColor("DE", "red", "acidic"),
+            SymbolColor("PAWFLIMV", "black", "hydrophobic")
+        ],
+        alphabet=seq.unambiguous_protein_alphabet
+    )
+    '''
     options.color_scheme = w.colorscheme.chemistry
     options.unit_name = "probability"
     options.yaxis_label = "Probability"
