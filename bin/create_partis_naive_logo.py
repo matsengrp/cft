41b7de602aebbb0495d77aaf4060ccc468e30f9c
eharkins <eli.harkins@gmail.com>
2019-03-18 18:55:20 -0700

    clean up bin/create_partis_naive_logo.py

--- bin/create_partis_naive_logo.py
+++ bin/create_partis_naive_logo.py
@@ -5,7 +5,7 @@ import subprocess
 import weblogolib as w
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Create logo to represent alternative-annotations naive probabilities rom partition-cluster-annotations.csv.")
+    parser = argparse.ArgumentParser(description="Create logo plot to represent uncertainty associated with alternative naive sequences.")
     parser.add_argument(
         'input_fasta_path', type=str,
         help="Path to file with all potential partis naive seqs; their frequency in this file should represent their probability.")
@@ -15,35 +15,13 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     
-    with open(args.output_base + ".fasta", "rU") as f:
+    with open(args.input_fasta_path, "rU") as f:
         seqs = w.read_seq_data(f)
     data = w.LogoData.from_seqs(seqs)
-    subprocess.check_call(("rm " + args.output_base + ".fasta").split())
+    subprocess.check_call(("rm " + args.input_fasta_path).split())
 
     options = w.LogoOptions()
-    '''
-    SymbolColor wont import
-    chemistry = w.colorscheme.ColorScheme(
-        [
-            w.colorscheme.SymbolColor("GSTYC", "green", "polar"),
-            w.colorscheme.SymbolColor("NQ", "purple", "neutral"),
-            w.colorscheme.SymbolColor("KRH", "blue", "basic"),
-            w.colorscheme.SymbolColor("DE", "red", "acidic"),
-            w.colorscheme.SymbolColor("PAWFLIMV", "black", "hydrophobic")
-        ],
-        alphabet=w.seq.unambiguous_protein_alphabet
-    )
-    bloom = ColorScheme(
-        [
-            SymbolColor("GSTYC", "green", "polar"),
-            SymbolColor("NQ", "purple", "neutral"),
-            SymbolColor("KRH", "blue", "basic"),
-            SymbolColor("DE", "red", "acidic"),
-            SymbolColor("PAWFLIMV", "black", "hydrophobic")
-        ],
-        alphabet=seq.unambiguous_protein_alphabet
-    )
-    '''
+    
     options.color_scheme = w.colorscheme.chemistry
     options.unit_name = "probability"
     options.yaxis_label = "Probability"
