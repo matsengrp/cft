02b9b3178d108ad18a3bbc3eb7e08cffe0b2bc2c
eharkins <eli.harkins@gmail.com>
2019-12-18 09:47:23 -0800

    black site_scons code

--- site_scons/backtrans_align.py
+++ site_scons/backtrans_align.py
@@ -7,9 +7,9 @@ import os
 import sys
 
 # submodule checkout; this is needed for bin/process_partis.py)
-default_partis_path = os.path.join(os.getcwd(), 'partis')
-partis_path = os.environ.get('PARTIS', default_partis_path)
-sys.path.append(os.path.join(partis_path, 'python'))
+default_partis_path = os.path.join(os.getcwd(), "partis")
+partis_path = os.environ.get("PARTIS", default_partis_path)
+sys.path.append(os.path.join(partis_path, "python"))
 import utils as partisutils
 
 # What follows is a rather convoluted backtranslation strategy for sequence alignment.
@@ -20,32 +20,39 @@ import utils as partisutils
 # So there's a bunch of housekeeping here in making sure all of this is the case for our final backtrans-align
 # step.
 
+
 def add(env, w, options):
-    if options['preserve_indels']:
+    if options["preserve_indels"]:
+
         @w.add_target()
         def translated_inseqs_(outdir, c):
             return env.Command(
-                [os.path.join(outdir, "translated_inseqs.fa"), os.path.join(outdir, 'inseqs_trimmed.fa')],
-                [c['partis_metadata'], c['inseqs']],
-                'translate_seqs.py $SOURCES $TARGET -t ${TARGETS[1]}')
+                [
+                    os.path.join(outdir, "translated_inseqs.fa"),
+                    os.path.join(outdir, "inseqs_trimmed.fa"),
+                ],
+                [c["partis_metadata"], c["inseqs"]],
+                "translate_seqs.py $SOURCES $TARGET -t ${TARGETS[1]}",
+            )
 
         @w.add_target()
         def translated_inseqs(outdir, c):
-            return c['translated_inseqs_'][0]
+            return c["translated_inseqs_"][0]
 
         @w.add_target()
         def trimmed_inseqs(outdir, c):
-            return c['translated_inseqs_'][1]
+            return c["translated_inseqs_"][1]
 
         @w.add_target()
         def aligned_translated_inseqs(outdir, c):
             return env.SRun(
                 os.path.join(outdir, "aligned_translated_inseqs.fa"),
-                c['translated_inseqs'],
+                c["translated_inseqs"],
                 # Replace stop codons with X, or muscle inserts gaps, which messed up seqmagick backtrans-align below
                 # Note that things will break down at backtrans-align if any seq ids have * in them...
-                'sed \'s/\*/X/g\' $SOURCE | muscle -in /dev/stdin -out $TARGET 2> $TARGET-.log',
-                srun_args='`alignment_srun_args.py $SOURCE`')
+                "sed 's/\*/X/g' $SOURCE | muscle -in /dev/stdin -out $TARGET 2> $TARGET-.log",
+                srun_args="`alignment_srun_args.py $SOURCE`",
+            )
 
         # Sort the sequences to have the same order, so that seqmagick doesn't freak out
 
@@ -53,22 +60,24 @@ def add(env, w, options):
         def sorted_inseqs(outdir, c):
             return env.Command(
                 os.path.join(outdir, "sorted_inseqs.fa"),
-                c['trimmed_inseqs'],
-                'seqmagick convert --sort name-asc $SOURCE $TARGET')
+                c["trimmed_inseqs"],
+                "seqmagick convert --sort name-asc $SOURCE $TARGET",
+            )
 
         @w.add_target()
         def sorted_aligned_translated_inseqs(outdir, c):
             return env.Command(
                 os.path.join(outdir, "sorted_aligned_translated_inseqs.fa"),
-                c['aligned_translated_inseqs'],
-                'seqmagick convert --sort name-asc $SOURCE $TARGET')
+                c["aligned_translated_inseqs"],
+                "seqmagick convert --sort name-asc $SOURCE $TARGET",
+            )
 
     # Now, finally, we can backtranslate
 
     @w.add_target()
     def aligned_inseqs(outdir, c):
-        aligned_inseqs_fname = os.path.join(outdir, 'aligned_inseqs.fa')
-        if options['preserve_indels']:
+        aligned_inseqs_fname = os.path.join(outdir, "aligned_inseqs.fa")
+        if options["preserve_indels"]:
             msg = """
                     Aligning partis cluster sequences because --preserve-indels was passed
                     (either explicitly or by passing --match-indels-in-uid).
@@ -76,13 +85,11 @@ def add(env, w, options):
                     Check this alignment using information from partis about indels in these sequences
                     before relying on this alignment for inference and moving forward with analysis of these clonal families.
                 """
-            warnings.warn(partisutils.color('red',msg))
+            warnings.warn(partisutils.color("red", msg))
             return env.Command(
                 aligned_inseqs_fname,
-                [c['sorted_aligned_translated_inseqs'], c['sorted_inseqs']],
-                'seqmagick backtrans-align -a warn $SOURCES -o $TARGET')
+                [c["sorted_aligned_translated_inseqs"], c["sorted_inseqs"]],
+                "seqmagick backtrans-align -a warn $SOURCES -o $TARGET",
+            )
         else:
-            return env.Command(
-                aligned_inseqs_fname,
-                c['inseqs'],
-                'cp $SOURCE $TARGET')
+            return env.Command(aligned_inseqs_fname, c["inseqs"], "cp $SOURCE $TARGET")
--- site_scons/options.py
+++ site_scons/options.py
@@ -1,127 +1,170 @@
 import SCons.Script as Script
 
-Script.AddOption('--infiles',
-        dest='infiles',
-        metavar='FILE_LIST',
-        default="tests/test.yaml",
-        help="""Specify ':' separated list of partis output directories to process on; if full path not specified,
+Script.AddOption(
+    "--infiles",
+    dest="infiles",
+    metavar="FILE_LIST",
+    default="tests/test.yaml",
+    help="""Specify ':' separated list of partis output directories to process on; if full path not specified,
         assumed to be in --base-datapath. Dataset names will be assined in relation to this --base-datapath
-        if present. Note: symlinks may not work properly here unless they point to things in base-datapath also.""")
+        if present. Note: symlinks may not work properly here unless they point to things in base-datapath also.""",
+)
 
-Script.AddOption('--depth',
-        dest="depth",
-        help="""How many clonal families should we process per unseeded sample and locus? Defaults to 20,
-        unless this is a test run in which case it defaults to 3.""")
+Script.AddOption(
+    "--depth",
+    dest="depth",
+    help="""How many clonal families should we process per unseeded sample and locus? Defaults to 20,
+        unless this is a test run in which case it defaults to 3.""",
+)
 
-Script.AddOption('--process-all-partis-partition-steps',
-        dest="process_all_partis_partition_steps",
-        action="store_true",
-        help="""If this is set, all partition steps are processed. By default only the best partis partition step is processed (cp.i_best).""")
+Script.AddOption(
+    "--process-all-partis-partition-steps",
+    dest="process_all_partis_partition_steps",
+    action="store_true",
+    help="""If this is set, all partition steps are processed. By default only the best partis partition step is processed (cp.i_best).""",
+)
 
-Script.AddOption('--only-seeds',
-        dest='only_seeds',
-        metavar='LIST',
-        default=None,
-        help="""Specify ':' separated list of partis seeds to process only those seed containing clusters.""")
+Script.AddOption(
+    "--only-seeds",
+    dest="only_seeds",
+    metavar="LIST",
+    default=None,
+    help="""Specify ':' separated list of partis seeds to process only those seed containing clusters.""",
+)
 
-Script.AddOption('--match-indels-in-uid',
-        dest='match_indels_in_uid',
-        default=None,
-        help="""process only sequences matching the one indel in the sequence corresponding to the uid passed here in the annotation chosen in choose_cluster()""")
+Script.AddOption(
+    "--match-indels-in-uid",
+    dest="match_indels_in_uid",
+    default=None,
+    help="""process only sequences matching the one indel in the sequence corresponding to the uid passed here in the annotation chosen in choose_cluster()""",
+)
 
-Script.AddOption('--ignore-seed-indels',
-        dest='ignore_seed_indels',
-        action="store_true",
-        help="""If --match-indels-in-uid has not been set, this allows processing of a seed cluster (without filtering) where there is an indel in the seed sequence.""")
-        
-Script.AddOption('--asr-progs',
-        dest='asr_progs',
-        metavar='LIST',
-        default='dnaml',
-        help="""Specify ':' separated list of ancestral state reconstruction programs to run. Options are `dnaml` and
-        `dnapars`. Defaults to running dnaml.""")
+Script.AddOption(
+    "--ignore-seed-indels",
+    dest="ignore_seed_indels",
+    action="store_true",
+    help="""If --match-indels-in-uid has not been set, this allows processing of a seed cluster (without filtering) where there is an indel in the seed sequence.""",
+)
 
-Script.AddOption('--prune-strategies',
-        dest='prune_strategies',
-        metavar='LIST',
-        default='min_adcl:seed_lineage',
-        help="""Specify ':' separated list of pruning strategies. Options are 'min_adcl' and 'seed_lineage'.
-        Defaults to both.""")
+Script.AddOption(
+    "--asr-progs",
+    dest="asr_progs",
+    metavar="LIST",
+    default="dnaml",
+    help="""Specify ':' separated list of ancestral state reconstruction programs to run. Options are `dnaml` and
+        `dnapars`. Defaults to running dnaml.""",
+)
 
-Script.AddOption('--test',
-        dest='test_run',
-        action='store_true',
-        default=False,
-        help="Setting this flag does a test run for just a couple of seeds")
+Script.AddOption(
+    "--prune-strategies",
+    dest="prune_strategies",
+    metavar="LIST",
+    default="min_adcl:seed_lineage",
+    help="""Specify ':' separated list of pruning strategies. Options are 'min_adcl' and 'seed_lineage'.
+        Defaults to both.""",
+)
 
-Script.AddOption('--dataset-tag',
-        dest='dataset_tag',
-        metavar='TAG',
-        help="Adds a tag to the beginning of the automatically generated dataset ids")
+Script.AddOption(
+    "--test",
+    dest="test_run",
+    action="store_true",
+    default=False,
+    help="Setting this flag does a test run for just a couple of seeds",
+)
 
-Script.AddOption('--outdir',
-        dest='outdir',
-        metavar='DIR',
-        default='output',
-        help="Directory in which to output results; defaults to `output`")
+Script.AddOption(
+    "--dataset-tag",
+    dest="dataset_tag",
+    metavar="TAG",
+    help="Adds a tag to the beginning of the automatically generated dataset ids",
+)
 
-Script.AddOption('--lazy-metadata',
-        dest='lazy_metadata',
-        action='store_true',
-        default=False,
-        help="""Turns off the default `AlwaysBuild` setting on the various json metadata targets. Without `AlwaysBuild`, metadata
+Script.AddOption(
+    "--outdir",
+    dest="outdir",
+    metavar="DIR",
+    default="output",
+    help="Directory in which to output results; defaults to `output`",
+)
+
+Script.AddOption(
+    "--lazy-metadata",
+    dest="lazy_metadata",
+    action="store_true",
+    default=False,
+    help="""Turns off the default `AlwaysBuild` setting on the various json metadata targets. Without `AlwaysBuild`, metadata
         files may not update properly if any of the SConstruct code changed. However, this can be useful for improving build
         time when debugging, or when only code in scripts has changed. If `--lazy-metadata` is used, it's best to run again
-        before using any of the output metadata.json files.""")
+        before using any of the output metadata.json files.""",
+)
 
-Script.AddOption('--inferred-naive-name',
-        dest='inferred_naive_name',
-        default='inferred_naive',
-        help="""What do we call the partis-inferred naive sequence when we inject it among the other (input) sequences.""")
+Script.AddOption(
+    "--inferred-naive-name",
+    dest="inferred_naive_name",
+    default="inferred_naive",
+    help="""What do we call the partis-inferred naive sequence when we inject it among the other (input) sequences.""",
+)
 
-Script.AddOption('--fasttree-png',
-        dest='fasttree_png',
-        action='store_true',
-        default=False,
-        help="Setting this flag builds fasttree pngs for small-med sized trees")
+Script.AddOption(
+    "--fasttree-png",
+    dest="fasttree_png",
+    action="store_true",
+    default=False,
+    help="Setting this flag builds fasttree pngs for small-med sized trees",
+)
 
-Script.AddOption('--write-linearham-yaml-input',
-        dest='write_linearham_yaml_input',
-        action='store_true',
-        default=False,
-        help="""Setting this flag writes a partis partition file for the pruned cluster annotation and adds necssary info to use
-        said file as input to linearham.""")
+Script.AddOption(
+    "--write-linearham-yaml-input",
+    dest="write_linearham_yaml_input",
+    action="store_true",
+    default=False,
+    help="""Setting this flag writes a partis partition file for the pruned cluster annotation and adds necssary info to use
+        said file as input to linearham.""",
+)
 
-Script.AddOption('--preserve-indels',
-        dest='preserve_indels',
-        action='store_true',
-        default=False,
-        help="""Setting this flag assumes there are indels and does not use indel reversed input sequences (indel reversed sequences are used by default). Instead, partis 'input_seqs' key sequences are aligned and used as the cluster sequences.""")
+Script.AddOption(
+    "--preserve-indels",
+    dest="preserve_indels",
+    action="store_true",
+    default=False,
+    help="""Setting this flag assumes there are indels and does not use indel reversed input sequences (indel reversed sequences are used by default). Instead, partis 'input_seqs' key sequences are aligned and used as the cluster sequences.""",
+)
+
 
 def get_options(env):
-    test_run, dataset_tag, match_indels_in_uid = env.GetOption("test_run"), env.GetOption('dataset_tag'), env.GetOption('match_indels_in_uid')
-    user_tag = (dataset_tag if dataset_tag else '')
-    test_tag = ('test' if test_run else '')
-    match_indels_in_uid_tag = ('match-indels-in-uid-{}'.format(match_indels_in_uid) if match_indels_in_uid else '')
-    tag = '_'.join(filter(None, [user_tag, test_tag, match_indels_in_uid_tag]))
+    test_run, dataset_tag, match_indels_in_uid = (
+        env.GetOption("test_run"),
+        env.GetOption("dataset_tag"),
+        env.GetOption("match_indels_in_uid"),
+    )
+    user_tag = dataset_tag if dataset_tag else ""
+    test_tag = "test" if test_run else ""
+    match_indels_in_uid_tag = (
+        "match-indels-in-uid-{}".format(match_indels_in_uid)
+        if match_indels_in_uid
+        else ""
+    )
+    tag = "_".join(filter(None, [user_tag, test_tag, match_indels_in_uid_tag]))
     return dict(
-        infiles = env.GetOption('infiles').split(':'),
-        depth = int(env.GetOption('depth') or (3 if test_run else 30)),
-        process_all_partis_partition_steps = env.GetOption('process_all_partis_partition_steps'),
-        only_seeds = env.GetOption('only_seeds').split(':') if env.GetOption('only_seeds') is not None else None,
-        ignore_seed_indels = env.GetOption('ignore_seed_indels'),
-        match_indels_in_uid = env.GetOption('match_indels_in_uid'),
-        test_run = test_run,
-        asr_progs = env.GetOption('asr_progs').split(':'),
-        prune_strategies = env.GetOption('prune_strategies').split(':'),
-        dataset_tag = tag,
-        always_build_metadata = not env.GetOption('lazy_metadata'),
-        inferred_naive_name = env.GetOption('inferred_naive_name'),
-        outdir_base = env.GetOption('outdir'),
-        fasttree_png = env.GetOption('fasttree_png'),
-        preserve_indels = env.GetOption('preserve_indels') or (match_indels_in_uid is not None),
-        write_linearham_yaml_input = env.GetOption('write_linearham_yaml_input'))
-
-
-
-
+        infiles=env.GetOption("infiles").split(":"),
+        depth=int(env.GetOption("depth") or (3 if test_run else 30)),
+        process_all_partis_partition_steps=env.GetOption(
+            "process_all_partis_partition_steps"
+        ),
+        only_seeds=env.GetOption("only_seeds").split(":")
+        if env.GetOption("only_seeds") is not None
+        else None,
+        ignore_seed_indels=env.GetOption("ignore_seed_indels"),
+        match_indels_in_uid=env.GetOption("match_indels_in_uid"),
+        test_run=test_run,
+        asr_progs=env.GetOption("asr_progs").split(":"),
+        prune_strategies=env.GetOption("prune_strategies").split(":"),
+        dataset_tag=tag,
+        always_build_metadata=not env.GetOption("lazy_metadata"),
+        inferred_naive_name=env.GetOption("inferred_naive_name"),
+        outdir_base=env.GetOption("outdir"),
+        fasttree_png=env.GetOption("fasttree_png"),
+        preserve_indels=env.GetOption("preserve_indels")
+        or (match_indels_in_uid is not None),
+        write_linearham_yaml_input=env.GetOption("write_linearham_yaml_input"),
+    )
